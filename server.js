625bde58095f3d02f2e54fe0b3cebe85990c05c4
Neo <scottfinamore@gmail.com>
2024-04-23 17:31:03 -0400

    App setup complete, ready to add route handlers to db.js

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const apiRouter = require("./server/api");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+app.use(cors());
+app.use("/api", apiRouter);
 
 module.exports = app;
-app.use(express.static(__dirname));
 
 /* Do not change the following line! It is required for testing and allowing
  *  the frontend application to interact as planned with the api server
@@ -14,8 +22,7 @@ const PORT = process.env.PORT || 4001;
 // Add middware for parsing request bodies here:
 
 // Mount your existing apiRouter below at the '/api' path.
-const apiRouter = require("./server/api");
-
+const { json } = require("express/lib/response");
 // This conditional is here for testing purposes:
 if (!module.parent) {
   // Add your code to start the server listening at PORT below:
