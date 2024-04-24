d6d9be300ddd1dae94d988ec83a9eaca5cd72f72
Neo <scottfinamore@gmail.com>
2024-04-24 19:05:01 -0400

    Minions routes complete

--- server/api.js
+++ server/api.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 const apiRouter = express.Router();
 const db = require("./db");
 
@@ -7,5 +8,42 @@ apiRouter.get("/minions", (req, res, next) => {
   const minions = db.getAllFromDatabase("minions");
   res.status(200).json(minions);
 });
+
+apiRouter.post("/minons", (req, res, next) => {
+  const newMinion = req.body;
+  const createdMinion = db.addToDatabase("minions", newMinion);
+  res.status(201).json(createdMinion);
+});
+
+apiRouter.get("/minions/:minionId", (req, res, next) => {
+  const minion = db.getFromDatabaseById("minions", req.params.minionId);
+  if (minion) {
+    res.status(200).json(minion);
+  } else {
+    res.status(404).send("Minion not found");
+  }
+});
+
+apiRouter.put("/minions/:minionId", (req, res, next) => {
+  const updatedMinion = req.body;
+  updatedMinion.id = req.params.minionId;
+  const result = db.updateInstanceInDatabase("minions", updatedMinion);
+  if (result) {
+    res.status(200).json(result);
+  } else {
+    res.status(404).send("Minion not found");
+  }
+});
+
+apiRouter.delete("/minions/:minionId", (req, res, next) => {
+  const deleted = db.deleteAllFromDatabase("minons", req.params.minionId);
+  if (deleted) {
+    res.status(204).send();
+  } else {
+    res.status(404).send("Minion not found");
+  }
+});
+
+// Routes for /api/ideas
 
 module.exports = apiRouter;
