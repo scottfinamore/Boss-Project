53b3720e20315fdd1fdbaa439f514d96ebc777b8
Neo <scottfinamore@gmail.com>
2024-04-24 13:59:01 -0400

    First route created, GET route for minions to get an array of minions

--- server/api.js
+++ server/api.js
@@ -1,6 +1,11 @@
-const express = require('express');
+const express = require("express");
 const apiRouter = express.Router();
+const db = require("./db");
 
-
+// Routes for /api/minions
+apiRouter.get("/minions", (req, res, next) => {
+  const minions = db.getAllFromDatabase("minions");
+  res.status(200).json(minions);
+});
 
 module.exports = apiRouter;
